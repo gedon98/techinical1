2e937a3cffd417017596d04b1d9eb8ed09e27a9f
gedon98 <39514657+gedon98@users.noreply.github.com>
2018-05-22 13:59:06 +0100

    Initial Commit

---
+++ messageproc/src/main/java/tech/messageproc/App.java
@@ -0,0 +1,13 @@
+package tech.messageproc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
